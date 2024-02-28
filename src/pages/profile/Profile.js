6875d06b348db4ad5e668dff8d099da8ca68280b
marwa <boukhrismarwa06@gmail.com>
2024-02-28 11:17:09 +0100

    first

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -2,7 +2,7 @@ import React,{useRef,useState,useEffect} from 'react';
 import { useNavigate } from 'react-router-dom';
 import {useSelector, useDispatch} from 'react-redux';
 import {getDownloadURL, getStorage,ref, uploadBytesResumable} from "firebase/storage";
-import {app} from "../../firebase";
+
 import {deleteUser,updateUser,getuser } from "../../redux/slices/userSlice";
 import { ToastContainer, toast } from 'react-toastify';
 import { MdDelete } from "react-icons/md";
@@ -22,30 +22,7 @@ const Profile = () => {
     const [imagePerCent,setImagePerCent]=useState(0);
     const [imageError,setImageError] = useState(false);
 
-   //upload image firebase
-    useEffect(()=>{
-        if (image){
-            handleFileUpload(image);
-        }
-    },[image]);
-    const handleFileUpload = async(image)=>{
-        const storage = getStorage(app);
-        const fileName = new Date().getTime()+ image.name;  //create a unique name for the image to be stored in the firebase cloud storage bucket
-        const storageRef = ref(storage, fileName);
-        const uploadTask = uploadBytesResumable(storageRef, image);
-        uploadTask.on('state_changed', (snapshot) => {
-            const progress =
-               (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-                setImagePerCent(Math.round(progress));
-        },
-        (error)=>{
-            setImageError(true)
-        },
-        ()=>{
-            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => 
-                setFormData({...formData,profilePicture:downloadURL,_id: user._id})
-        );});
-    };
+   
     const handleChange = (e) =>{
         setUpdated({...updated,[e.target.name]: e.target.value})
     }
@@ -95,10 +72,6 @@ const Profile = () => {
                 />
                 <img className='profilepic' src={formData.profilePicture || profilePicture} alt='profile picture'
                 onClick={()=>fileRef.current.click()} />
-                <p className='uploadMessage'>{imageError ? (
-                    <span>Error uploading image (file size must be less than 2MB)</span>) : imagePerCent>0 && imagePerCent<100 ? (<span>{`uploading:  ${imagePerCent} % `}</span>)
-                    : imagePerCent ===100 ? (<span>Image upload successfully</span>) : ''}
-                </p>
                 <input className='inputProfile' type='text' name='name' placeholder='User Name' defaultValue={user.name}
                 onChange={handleChange}/>
                 <input className='inputProfile' type='email' name='email' placeholder='Email' defaultValue={user.email}
